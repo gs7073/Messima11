c9c876395ba5db0b249730c26fa8e0897ea9947a
Cyber_Mamriot <gs7073@bs.amalnet.k12.il>
2026-01-11 19:49:34 +0200

    commit 5

---
+++ app/src/main/java/com/example/messima11/CreditsActivity.java
@@ -0,0 +1,13 @@
+package com.example.messima11;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
